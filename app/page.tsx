84ceea7a3c8edcd825574777814685b76b7ea6ec
Jose Fernando Hower Barbosa <jose.hower@lingvano.com>
2024-04-15 10:57:42 +0200

    Add code after know how

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-import Image from 'next/image';
+'use client';
+import { Environment, OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { useControls } from 'leva';
+
+function Sphere() {
+  const { roughness } = useControls({
+    roughness: { value: 1, min: 0, max: 1 },
+  });
+
+  return (
+    <mesh>
+      <sphereGeometry args={[0.35, 10, 20]} />
+      <meshStandardMaterial
+        metalness={1}
+        roughness={roughness}
+        color={'green'}
+      />
+    </mesh>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="text-center p-24 text-2xl">This is a R3Fiber Intro</main>
+    <main className="text-center p-24 text-2xl h-screen">
+      <Canvas shadows camera={{ position: [0, 0, 4.5], fov: 50 }}>
+        <Environment preset={'sunset'} background />
+        <Sphere />
+        <OrbitControls />
+      </Canvas>
+    </main>
   );
 }
